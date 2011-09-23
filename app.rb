9cb726fd42b38846e57af6976eda6bd9a51b33b5
Brian Ryckbost <bryckbost@gmail.com>
2011-09-23 09:21:00 -0400

    Add a friendly name helper. Add page and search routes.

--- app.rb
+++ app.rb
@@ -2,12 +2,40 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/mongo'
+require 'active_support/core_ext/string/inflections'
 MultiJson.engine = :yajl
+
+if File.exist?('config/application.yml')
+  config = YAML.load_file('config/application.yml')
+  config.each{|k,v| ENV[k] = v }
+end
 
 set :mongo, ENV['MONGOHQ_URL']
 
+helpers do
+  def friendly_name(liquor_name)
+    liquor_name.downcase.titleize
+  end
+end
+
+# only set the random liquor for non /api endpoints
+before /^\/((?!api).*)/ do
+  liquors = mongo["liquors"].find()
+  @header_liquor = liquors.skip(rand(liquors.count)).limit(1).first
+end
+
 get '/' do
-  @liquors = mongo["liquors"].find().to_a
+  @liquors = mongo["liquors"].find().limit(150).to_a
+  erb :index
+end
+
+get '/page/:page' do
+  @liquors = mongo["liquors"].find().skip(150 * params[:page].to_i).limit(150).to_a
+  erb :index
+end
+
+get '/search' do
+  @liquors = mongo["liquors"].find({"BRAND NAME" => /#{params[:q]}/i}).to_a
   erb :index
 end
 
@@ -18,6 +46,6 @@ end
 
 get '/api/search' do
   content_type :json
-  MultiJson.encode mongo["liquors"].find({"BRAND NAME" => /#{params[:q]}/}).to_a
+  MultiJson.encode mongo["liquors"].find({"BRAND NAME" => /#{params[:q]}/i}).to_a
 end
 
